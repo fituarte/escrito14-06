cd53b1ecded94c6865212c0210e63813985dc415
fituarte <fiammaituarte@outlook.com>
2024-06-14 21:09:10 -0300

    funcion de calcular promedios

--- pruebaFunciones.php
+++ pruebaFunciones.php
@@ -87,4 +87,85 @@ $inventario = calcularValorInventario($productosValor);
 echo $inventario;
 
 
+
+
+$filtrarProductos = [
+    ["nombre" => "championes", "cantidad" => 40, "valor" => "500", "modelo" => "victor"],
+    ["nombre" => "pantalon", "cantidad" => 40, "valor" => "600", "modelo" => "victor"],
+    ["nombre" => "celular", "cantidad" => 40, "valor" => "700", "modelo" => "victor"],
+    ["nombre" => "tv", "cantidad" => 40, "valor" => "800", "modelo" => "victor"]
+];
+
+function filtrarProductosPorValor($productos, $valorMinimo) {
+    $productosFiltrados = array();
+
+    foreach ($productos as $producto) {
+        $costo = $producto["valor"];
+   $valor = floatval($costo);
+        if ($valor > $valorMinimo) {
+            $productosFiltrados[] = $producto;
+        }
+    }
+
+    return $productosFiltrados;
+}
+
+$resultadoFiltrado = filtrarProductosPorValor($productos, 80);
+
+echo "<h2>Productos con valor mayor a 80:</h2>";
+echo "<ul>";
+foreach ($resultadoFiltrado as $producto) {
+    echo "<li>{$producto['nombre']} - Valor: {$producto['valor']}</li>";
+}
+echo "</ul>";
+
+
+$productos = [
+    ["nombre" => "championes", "cantidad" => 40, "valor" => "500", "modelo" => "adidas"],
+    ["nombre" => "pantalon", "cantidad" => 40, "valor" => "600", "modelo" => "reebok"],
+    ["nombre" => "celular", "cantidad" => 40, "valor" => "700", "modelo" => "samsung"],
+    ["nombre" => "tv", "cantidad" => 40, "valor" => "800", "modelo" => "panavox"]
+];
+
+$modelos_disponibles = array();
+
+foreach ($productos as $producto) {
+    $modelo = $producto['modelo'];
+    if (!in_array($modelo, $modelos_disponibles)) {
+        $modelos_disponibles[] = $modelo;
+    }
+}
+
+echo "<h2>Modelos Disponibles:</h2>";
+echo "<ul>";
+foreach ($modelos_disponibles as $modelo) {
+    echo "<li>$modelo</li>";
+}
+echo "</ul>";
+
+$productosPromedio = [
+    ["nombre" => "championes", "cantidad" => 40, "valor" => "500", "modelo" => "adidas"],
+    ["nombre" => "pantalon", "cantidad" => 40, "valor" => "600", "modelo" => "reebok"],
+    ["nombre" => "celular", "cantidad" => 40, "valor" => "700", "modelo" => "samsung"],
+    ["nombre" => "tv", "cantidad" => 40, "valor" => "800", "modelo" => "panavox"]
+];
+
+function calcularPromedio($productosPromedio){
+    $suma = 0;
+    $promedio = 0;
+
+  foreach($productosPromedio as $producto){
+    $costo = $producto["valor"];
+   $valor = floatval($costo);
+   $suma += $valor;
+   $promedio = $suma / count($productosPromedio);
+}
+
+return $promedio;
+}
+
+$promedio = calcularPromedio($productosPromedio);
+
+echo  "el promedio es: " . $promedio;
+
 ?>
